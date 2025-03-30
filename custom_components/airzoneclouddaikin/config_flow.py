82656190442df178481e08af26f83aea945b489c
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2025-03-30 21:30:34 +0200

    Update config_flow.py

--- custom_components/airzoneclouddaikin/config_flow.py
+++ custom_components/airzoneclouddaikin/config_flow.py
@@ -5,15 +5,15 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 from homeassistant.helpers import config_validation as cv
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from .const import DOMAIN
-from .airzone_api import AirzoneAPI
 
 _LOGGER = logging.getLogger(__name__)
 
+# Add scan_interval option (default 30 seconds, minimum 10)
 DATA_SCHEMA = vol.Schema({
     vol.Required(CONF_USERNAME): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional("scan_interval", default=30): vol.All(vol.Coerce(int), vol.Range(min=10)),
     vol.Optional("force_hvac_mode_auto", default=False): bool,
 })
 
@@ -25,6 +25,9 @@ class AirzoneConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle a flow initiated by the user."""
         errors = {}
         if user_input is not None:
+            # Validate credentials by attempting a login
+            from homeassistant.helpers.aiohttp_client import async_get_clientsession
+            from .airzone_api import AirzoneAPI
             session = async_get_clientsession(self.hass)
             api = AirzoneAPI(user_input[CONF_USERNAME], user_input[CONF_PASSWORD], session)
             if not await api.login():
