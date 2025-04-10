46cd09d1945f8d5b7a0c5f39b3ad991ce721c39f
eXPerience83 <16572400+eXPerience83@users.noreply.github.com>
2025-04-10 11:59:12 +0200

    Update switch.py

--- custom_components/airzoneclouddaikin/switch.py
+++ custom_components/airzoneclouddaikin/switch.py
@@ -48,14 +48,14 @@ class AirzonePowerSwitch(SwitchEntity):
 
     @property
     def device_info(self):
-        """Return device info to link this sensor to a device in Home Assistant."""
+        """Return device info to link this sensor to a device in Home Assistant.""" 
         return {
             "identifiers": {(DOMAIN, self._device_data.get("id"))},
             "name": self._device_data.get("name"),
             "manufacturer": "Daikin",
             "model": f"{self._device_data.get('brand', 'Unknown')} (PIN: {self._device_data.get('pin')})",
             "sw_version": self._device_data.get("firmware", "Unknown"),
-            "connections": {("mac", self._device_data.get("mac"))},  # MAC agregada
+            "connections": {("mac", self._device_data.get("mac"))} if self._device_data.get("mac") else None,
         }
 
     async def async_turn_on(self, **kwargs):
